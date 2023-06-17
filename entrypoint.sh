dc743ec73ea0b449070fc00f2082fc41da551b79
Khouloud <khouloudfrikha@gmail.com>
2023-06-17 16:04:42 +0100

    Ajouter README et persister le volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,9 @@ PGPASSWORD=$PG_PASSWORD PGHOST=$PG_HOST psql -U $PG_USER -c "GRANT ALL PRIVILEGE
 
 python manage.py makemigrations
 python manage.py migrate
+
+#pbkdf2_sha256$390000$otpWruZLf1FEoE9z7vAiJ6$+eXnAFCRDpaQ7YUpeHTEnfmzfHBIWABmn/KyVVJXzOM=
+
+PGPASSWORD=$PG_PASSWORD PGHOST=$PG_HOST psql -U $PG_USER -c "INSERT INTO public.auth_user(password,is_superuser,username,first_name,last_name,email,is_staff,is_active,date_joined) VALUES ('','1','khouloud','frikha','khouloud','khouloud@gmail.com','1','1','2023-04-30');" || true
+
 python manage.py runserver 0.0.0.0:8000 --noreload
